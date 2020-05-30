185d28273581397e2918a1bfbd07b8392f7a06a4
SaltFish <saltfish@whut.edu.cn>
2020-05-31 01:49:44 +0800

    优化了模板位置

--- NovelWebsite/settings.py
+++ NovelWebsite/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'NovelWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
