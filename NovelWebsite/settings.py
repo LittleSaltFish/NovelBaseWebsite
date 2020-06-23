9e6d51c4649731a09f039652677ffacb8446db55
SaltFish <saltfish@whut.edu.cn>
2020-06-24 02:32:37 +0800

    恶趣味小组件1.0版

--- NovelWebsite/settings.py
+++ NovelWebsite/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 # 富文本编辑器配置
 TINYMCE_DEFAULT_CONFIG = {
     #  'theme':'advanced',
